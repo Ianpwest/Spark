c85ebc427d7d9241a2b45ebe71f5e876baab175a
jamrreed <jamrreed@gmail.com>
2013-12-10 18:30:00 -0600

    Adding javascript and references.

---
+++ Spark/Scripts/SparkCreate.js
@@ -0,0 +1,18 @@
+﻿//*******************************************************************************************************//
+// Contains functions related to the spark creation page.                                                //
+//*******************************************************************************************************//
+
+function readUrl(input) {
+
+    document.getElementById("test").setAttribute("title", "Test2");
+    if (input.files && input.files[0]) 
+    {
+        var reader = new FileReader();
+        reader.onload = function (e) 
+        {
+            $('#imgPreview').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
