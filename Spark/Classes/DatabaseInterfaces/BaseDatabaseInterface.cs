8bfdb3c3aed6c7f345140a83459f417745c36d8b
jamrreed <jamrreed@gmail.com>
2013-12-15 19:40:25 -0600

    New database schema

---
+++ Spark/Classes/DatabaseInterfaces/BaseDatabaseInterface.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Spark.Classes.DatabaseInterfaces
+{
+    public static class BaseDatabaseInterface
+    {
+        // TODO - change this to the error logging entity model when the model is updated from the DB.
+        private static Spark.Models.sparkdbEntities m_dbErrorLog = new Models.sparkdbEntities();
+
+        public static void LogError(int accountId, string strMessage)
+        {
+            DateTime dtNow = DateTime.Now;
+        }
+
+        public static void LogError(int accountId, string strMessage, string strStackTrace)
+        {
+            DateTime dtNow = DateTime.Now;
+        }
+
+        public static void LogError(int accountId, string strMessage, string strControllerView, string strMethod, string strStackTrace, string strVariableName)
+        {
+            DateTime dtNow = DateTime.Now;
+        }
+    }
+}
