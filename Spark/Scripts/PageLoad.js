cb1e23d6f4482429ef57621ed7f8233f484dd5c7
Ian Weston <Ian.p.weston@gmail.com>
2013-12-17 20:53:22 -0600

    More styling, aligned older forms

--- Spark/Scripts/PageLoad.js
+++ Spark/Scripts/PageLoad.js
@@ -5,6 +5,7 @@
 
 $(document).ready(function ()
 {
+    //Display the page header unless it's the portal.
     if ($(".title").text() != "Portal")
     {
         $(".NavigationBarTitleP").text($(".title").text());
