bd82cfb2b515ca5a8c25c65b05138821536b463f
Ian Weston <Ian.p.weston@gmail.com>
2014-04-08 20:05:12 -0500

    randalls balls

--- Spark/Scripts/ArgumentExpansion.js
+++ Spark/Scripts/ArgumentExpansion.js
@@ -5,11 +5,6 @@
     getExpandedArgumentView(id);
 }
 
-function popupAddArgument(bAgree, nSparkId)
-{
-   getCreateArgumentView(bAgree, nSparkId);
-}
-
 function getExpandedArgumentView(id)
 {
     $.ajax({
@@ -24,23 +19,46 @@ function getExpandedArgumentView(id)
     });
 }
 
-function getCreateArgumentView(bAgree, nSparkId)
-{
-    $.ajax({
-        type: "Get",
-        datatype: 'html',
-        data: "bAgree=" + bAgree + "&nSparkId=" + nSparkId,
-        url: "/Spark/CreateArgument",
-        success: function (data)
-        {
-            $('.modal-content').html(data);
-        }
-    });
+function getCreateArgumentView(bAgree, nSparkId) {
+    //$.ajax({
+    //    type: "Get",
+    //    datatype: 'html',
+    //    data: "bAgree=" + bAgree + "&nSparkId=" + nSparkId,
+    //    url: "/Spark/CreateArgument",
+    //    success: function (data)
+    //    {
+    //        $('.modal-content').html(data);
+    //    }
+    //});
+
+    
+    //if (argumentContainerVisibility == "collapse")
+    //{
+    //    document.getElementById('ArgumentsContainer').style.visibility = "visible";
+    //    document.getElementById('CreateArgumentsForm').style.visibility = "collapse";
+    //}
+    //else
+    //{
+    //    document.getElementById('CreateArgumentsForm').style.visibility = "visible";
+    //    document.getElementById('ArgumentsContainer').style.visibility = "collapse";
+    //}
+
+    //Find the visibility of the current forms.
+    var argumentContainerVisibility = document.getElementById('ArgumentsContainer').style.visibility;
+    var argumentContainerVisibility = document.getElementById('CreateArgumentForm').style.visibility;
+
+    //Disable the agree disagree buttons
+    document.getElementById('buttonAgree').onclick = "null";
+    document.getElementById('buttonDisagree').onclick = "null";
+
+    //Hide the container and show the argument create form.
+    document.getElementById('CreateArgumentForm').style.visibility = "visible";
+    document.getElementById('ArgumentsContainer').style.visibility = "collapse";
 }
 
 function addArgument(isAgree, nSparkId)
 {
-    popupAddArgument(isAgree, nSparkId);
+    getCreateArgumentView(isAgree, nSparkId);
 }
 
 function addComment() {
